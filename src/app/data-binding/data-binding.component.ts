778248211ae91a9f3c984cc492be5ed2cb8ef9c7
Wiris Wernek <wiris.wernek@itixti.com.br>
2022-05-24 11:23:07 -0300

    adiciona event binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -8,11 +8,27 @@ import { Component, OnInit } from '@angular/core';
 export class DataBindingComponent implements OnInit {
   url: String = 'http://loiane.com';
   urlImagem = 'http://lorempixel.com.br/400/200/nature/';
+  valorAtual: string = '';
+  valorSalvo: any;
+  isMouseOver: boolean = false;
   constructor() {}
 
   ngOnInit(): void {}
 
   getValor(): number {
     return 1;
+  }
+
+  botaoClickado(): void {
+    alert('Botão clicado!');
+  }
+  onKeyUp(evento: KeyboardEvent): void {
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+  salvarValor(valor: any): void {
+    this.valorSalvo = valor.value;
+  }
+  OnMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
   }
 }
